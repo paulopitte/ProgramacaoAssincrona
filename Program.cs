edc38a82409f6541c1fefc4dd78c1e8d07cec0f0
Paulo Pitte <paulopitte@gmail.com>
2022-01-09 10:21:03 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProgramacaoAssincrona
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
